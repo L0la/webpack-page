df5399b0260ed7d9afbc8183c65b1e1ee6c2fe5c
L0la <torok.gabi@gmail.com>
2020-11-17 07:37:27 +0100

    Deployment script

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ document
 import "../index.html";
 import dataStr from "./date.js";
 import $ from "jquery";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/styles.css";
 import "bootstrap";
 $(document).ready(() => {
