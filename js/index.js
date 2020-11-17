4ad7d84f5702576c5c55bb374e2a380e8f00cd4e
L0la <torok.gabi@gmail.com>
2020-11-17 12:08:26 +0100

    Deployment script

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import dateStr from "./date.js";
+import $ from "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "../styles/styles.css";
+import "../index.html";
+$(document).ready(() => {
+    $('.js-content').html(`It's ${ dateStr() }`);
+    $('.js-modal').modal({
+        show: true,
+        keyboard: true
+    });
+});
